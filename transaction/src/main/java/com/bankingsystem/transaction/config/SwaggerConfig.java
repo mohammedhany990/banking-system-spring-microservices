9b279f39736b8b94593294cd1428710cffd637f7
Mohammed Hany <76411941+mohammedhany990@users.noreply.github.com>
2025-08-07 20:13:29 +0300

    Update SwaggerConfig.java

--- transaction/src/main/java/com/bankingsystem/transaction/config/SwaggerConfig.java
+++ transaction/src/main/java/com/bankingsystem/transaction/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class SwaggerConfig {
     @Bean
@@ -12,9 +13,9 @@ public class SwaggerConfig {
     {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Card Service")
+                        .title("Transaction Service")
                         .version("1.0")
-                        .description("Card Service API Documentation"));
+                        .description("Transaction Service API Documentation"));
     }
 }
 
